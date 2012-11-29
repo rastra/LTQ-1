a025f729c9b8f1352fea617f6cc73e04a9ed2c2c
rizabyyou <simpletohero.com>
2012-11-29 22:11:51 +0700

    Upload all file - rizabyyou

---
+++ javascript/questions-1.js
@@ -0,0 +1,89 @@
+var nTF = 0
+var nmulti = 10
+var nchoices = 5
+var qTF = new Array(nTF)
+var aTF = new Array(nTF)
+var pertanyaan = new Array(nmulti)
+var pilihan = new make_2d_array(nmulti,nchoices)
+var jawaban = new Array(nmulti)
+
+pertanyaan[1] = "Internet merupakan..."
+pilihan[1][1] = "sistem komputer<br>"
+pilihan[1][2] = "jaringan komputer<br>"
+pilihan[1][3] = "kelompok komputer<br>"
+pilihan[1][4] = "kumpulan komputer<br>"
+pilihan[1][5] = "rangkaian komputer<br>"
+jawaban[1] = 2
+
+pertanyaan[2] = "Istilah internet merupakan singkatan dari..."
+pilihan[2][1] = "<i>intercom network</i><br>"
+pilihan[2][2] = "<i>internet networking</i><br>"
+pilihan[2][3] = "<i>interconnected netlink</i><br>"
+pilihan[2][4] = "<i>international networking</i><br>"
+pilihan[2][5] = "<i>interconnected networking</i><br>"
+jawaban[2] = 5
+
+pertanyaan[3] = "Gabungan jaringan komputer di seluruh dunia yang membentuk suatu sistem jaringan informasi global adalah..."
+pilihan[3][1] = "LAN<br>"
+pilihan[3][2] = "MAN<br>"
+pilihan[3][3] = "internet<br>"
+pilihan[3][4] = "intranet<br>"
+pilihan[3][5] = "jaringan global<br>"
+jawaban[3] = 3
+
+pertanyaan[4] = "Internet pada awalnya hanya diperuntukkan bagi kalangan..."
+pilihan[4][1] = "guru<br>"
+pilihan[4][2] = "pengusaha<br>"
+pilihan[4][3] = "dosen dan mahasiswa<br>"
+pilihan[4][4] = "akademik dan militer<br>"
+pilihan[4][5] = "pengusaha dan akademik<br>"
+jawaban[4] = 4
+
+pertanyaan[5] = "Istilah internet muncul dengan ditemukannya protokol..."
+pilihan[5][1] = "FTP<br>"
+pilihan[5][2] = "WWW<br>"
+pilihan[5][3] = "TCP/IP<br>"
+pilihan[5][4] = "Ethernet<br>"
+pilihan[5][5] = "AppleTalk<br>"
+jawaban[5] = 3
+
+pertanyaan[6] = "Komputer yang bertugas menyediakan informasi yang dapat diakses oleh komputer lain melalui jaringan internet disebut..."
+pilihan[6][1] = "induk<br>"
+pilihan[6][2] = "<i>home</i><br>"
+pilihan[6][3] = "<i>client</i><br>"
+pilihan[6][4] = "<i>server</i><br>"
+pilihan[6][5] = "modem<br>"
+jawaban[6] = 4
+
+pertanyaan[7] = "Komputer di internet yang tidak menyediakan layanan informasi tetapi hanya bisa mengakses informasi dari komputer <i>server</i> dikenal dengan istilah..."
+pilihan[7][1] = "induk<br>"
+pilihan[7][2] = "<i>home</i><br>"
+pilihan[7][3] = "<i>client</i><br>"
+pilihan[7][4] = "<i>server</i><br>"
+pilihan[7][5] = "modem<br>"
+jawaban[7] = 3
+
+pertanyaan[8] = "Informasi atau tulisan yang terlihat pada layar monitor dinamakan... "
+pilihan[8][1] = "file<br>"
+pilihan[8][2] = "<i>soft copy</i><br>"
+pilihan[8][3] = "<i>hard copy</i><br>"
+pilihan[8][4] = "<i>copy paste</i><br>"
+pilihan[8][5] = "<i>print preview</i><br>"
+jawaban[8] = 2
+
+pertanyaan[9] = "Alat yang digunakan untuk menghantarkan dan menerima data dari sebuah PC ke sebuah PC yang lain melalui jaringan internet disebut..."
+pilihan[9][1] = "hub<br>"
+pilihan[9][2] = "FTP<br>"
+pilihan[9][3] = "<i>client</i><br>"
+pilihan[9][4] = "<i>server</i><br>"
+pilihan[9][5] = "modem<br>"
+jawaban[9] = 5
+
+pertanyaan[10] = "Modem yang mengubah sinyal analaog menjadi sinyal digital, digunakan untuk koneksi <i>dial up</i> melalui jaringan telepon disebut..."
+pilihan[10][1] = "modem DSL<br>"
+pilihan[10][2] = "wifi adapter<br>"
+pilihan[10][3] = "modem CDMA<br>"
+pilihan[10][4] = "modem kabel<br>"
+pilihan[10][5] = "modem analog<br>"
+jawaban[10] = 5
+
